f51fe9336b32ec2f58269d7dbee532ffca1a99cb
Dmytro Sergiienko <dmytro.sergiienko@beetroot.se>
2018-01-04 11:55:20 +0200

    Move slide content image into separate component.

--- views/meta-boxes/slide/components/content.php
+++ views/meta-boxes/slide/components/content.php
@@ -14,46 +14,8 @@
     </div>
     <div class="field__value">
         <textarea name="slide_content[<?= $i; ?>][text]"><?= array_get($slide, 'text'); ?></textarea>
-        <div class="slide-content__image slide-image">
-            <button type="button"
-                    id="insert-media-button"
-                    class="button insert-media add_media js-set-slide-image <?= array_get($slide, 'image') ? 'hidden' : ''; ?>"
-                    data-editor="content">
-                <span class="wp-media-buttons-icon"></span> <?= __('Add Media', 'lms-plugin'); ?>
-            </button>
 
-            <div class="js-slide-image <?= empty($slide['image']) ? 'hidden' : ''; ?>">
-                <a href="#" class="js-update-slide-image slide-image__thumbnail-wrapper">
-                    <img class="js-slide-image-thumbnail slide-image__thumbnail"
-                         src="<?= array_get($slide, 'thumbnail'); ?>"
-                    >
-                </a>
-
-                <div class="slide-image__help">
-                    <span><?= __('Click the image to edit or update', 'lms-plugin'); ?></span>
-                    <a href="#" class="js-remove-slide-image"><?= __('Remove image'); ?></a>
-                </div>
-
-                <label>
-                    <input type="checkbox"
-                           name="slide_content[<?= $i; ?>][image_as_background]"
-                           value="1"
-                           <?= checked(array_get($slide, 'image_as_background')); ?>>
-                    <?= __('Image as background', 'lms-plugin'); ?>
-                </label>
-            </div>
-
-            <input type="hidden"
-                   name="slide_content[<?= $i; ?>][thumbnail]"
-                   class="slide-thumbnail" value="<?= array_get($slide, 'thumbnail'); ?>"
-            >
-            <input type="hidden"
-                   name="slide_content[<?= $i; ?>][image]"
-                   class="slide-image" value="<?= array_get($slide, 'image'); ?>"
-            >
-
-        </div>
-
+        <?php include 'image.php'; ?>
     </div>
 
     <?php include 'advanced-settings.php'; ?>
