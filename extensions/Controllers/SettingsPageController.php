ff68d6d9162c2ff9b2bd0ededb0c111d00ce125b
Dmytro Sergiienko <dmytro.sergiienko@beetroot.se>
2018-01-30 13:08:23 +0200

    Add settings page.

--- extensions/Controllers/SettingsPageController.php
+++ extensions/Controllers/SettingsPageController.php
@@ -6,6 +6,17 @@ class SettingsPageController extends Controller
 {
     public function index()
     {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['settings'])) {
+            $this->save($_POST['settings']);
+        }
 
+        $settings = $this->plugin->getSettings();
+
+        $this->view('pages.settings.index', compact('settings'));
+    }
+
+    private function save($settings)
+    {
+        $this->plugin->setSettings($settings);
     }
 }
