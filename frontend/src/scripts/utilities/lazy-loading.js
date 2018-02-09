a7fa0a75166176bf83c64fc69542fb7a9da3a9b2
illetid <andrey.ovcharenko@beetroot.se>
2018-02-09 16:46:00 +0200

    Add lazy loading for images and background images.

---
+++ frontend/src/scripts/utilities/lazy-loading.js
@@ -0,0 +1,39 @@
+export const initLazyLoading = () => {
+    console.log('LAZY LOADING!!!!!!!!');
+    const elements = document.querySelectorAll('[data-src]');
+    const config = {
+        rootMargin: '0px 0px 50px 0px',
+        threshold: 0
+    };
+
+    let observer = new IntersectionObserver(function (entries, self) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log(`Image ${entry.target.src} is in the viewport!`);
+                preloadImage(entry.target);
+                // Stop watching and load the image
+                // self.unobserve(entry.target);
+            }
+        });
+    }, config);
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+
+    const preloadImage = (element) => {
+        console.log('OBSERVEEEEEEEEE', element);
+        const src = element.getAttribute('data-src');
+        const isImg = (element.nodeName.toLowerCase() === 'img');
+        if (!src) {
+            return;
+        }
+        if (isImg) {
+            element.src = src;
+        } else {
+            element.style.backgroundImage = `url(${src})`;
+        }
+
+
+    };
+};
