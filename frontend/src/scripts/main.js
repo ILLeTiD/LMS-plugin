4fa86f8b48cab3f2fdcd7ddfc2a147904955af2a
aovcharenko <andrey.ocharenko@beetroot.se>
2018-02-20 17:17:04 +0200

    Fix safari/ie image displaying bug (added intersection-observer polyfill).

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -6,11 +6,14 @@ var $ = window.jQuery;
 // import  'mediaelement';
 // Import libraries
 import 'izimodal';
-
+require('intersection-observer');
+import objectFitImages from 'object-fit-images';
 // Import custom modules
 import App from'./modules/app.js';
 
 (($) => {
+    IntersectionObserver.prototype.POLL_INTERVAL = 100;
+    objectFitImages();
     const app = new App();
     //var player = new MediaElementPlayer('#slide-control-player');
 })($);
