32a087550f142c5f87a79889ecfcb69a7c2390bc
Dmytro Sergiienko <d@sergiienko.pro>
2018-03-21 15:47:54 +0200

    Comment code for mailhog which is used only for dev envinronments.

--- extensions/actions.php
+++ extensions/actions.php
@@ -159,6 +159,7 @@ $action->add('wp_ajax_test', function () {
     dd(camel_case('resend_invite'));
 });
 
+/*
 $action->add('phpmailer_init', function ($phpmailer) {
     // Define that we are sending with SMTP
     $phpmailer->isSMTP();
@@ -185,6 +186,7 @@ $action->add('phpmailer_init', function ($phpmailer) {
     $phpmailer->From = 'noreply@fishy-minds.localhost';
     $phpmailer->FromName = 'WP DEV';
 });
+*/
 
 /**
  * Shortcodes.
