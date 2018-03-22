93726f4881079ae3420f78ad1727ad0f06865032
aovcharenko <andrey.ocharenko@beetroot.se>
2018-03-22 15:26:26 +0200

    Fixed videos size bugs.

--- templates/template-parts/regular-parts/grid-block.php
+++ templates/template-parts/regular-parts/grid-block.php
@@ -49,8 +49,7 @@ include 'section-settings.php';
                 <?php else : ?>
                     <video src="<?= $videoMedia; ?>"
                            style="max-width: 100%;"
-                           controls="controls" width="100%"
-                           height="100%"
+                           controls="controls"
                            class="lms-video-player    <?= $videoAutoplay ? 'autoplay' : ''; ?><?= $videoHideControls ? 'lms-video-player--disabled' : ''; ?>"></video>
 
                     <?php
